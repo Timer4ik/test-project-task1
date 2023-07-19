9b6701acc2d6d24592711e6a3ce2552ce4338242
timer4ik <timergalievadel@yandex.ru>
2023-07-19 07:59:33 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import TimerPage from "./pages/TimerPage"
+
+const App = () => {
+  return (
+    <div className="app">
+      <TimerPage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/TimerPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Timer from '../components/Timer'
+
+const TimerPage = () => {
+  return (
+    <div className='timer-page' >
+      <Timer />
+    </div>
+  )
+}
+
+export default TimerPage
